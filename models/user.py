eb0b3f9edac5d3d083173a3f4650dcd7c237dbc2
Major-L <sirlmoruri@gmail.com>
2023-03-23 03:12:25 -0700

    update user py

--- models/user.py
+++ models/user.py
@@ -1,27 +1,34 @@
 #!/usr/bin/python3
-'''
-    Implementation of the User class which inherits from BaseModel
-'''
+"""This is the user class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column
+from os import getenv
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import os
 
 
 class User(BaseModel, Base):
-    '''
-        Definition of the User class
-    '''
+    """ Represent a user for a MySQL database.
+    Attributes:
+        __tablename__: represents the table name, users
+        email: (sqlalchemy String): The user's email address.
+        password (sqlalchemy String): The user's password.
+        first_name (sqlalchemy String): The user's first name.
+        last_name (sqlalchemy String): The user's last name.
+        places (sqlalchemy relationship): The user-Place relationship.
+        reviews (sqlalchemy relationship): The user-Review relationship.
+    """
+
     __tablename__ = "users"
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", passive_deletes=True, backref="user")
-        reviews = relationship("Review", passive_deletes=True, backref="user")
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+        places = relationship("Place", backref="user", cascade="delete")
+        reviews = relationship("Review", backref="user", cascade="delete")
     else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
